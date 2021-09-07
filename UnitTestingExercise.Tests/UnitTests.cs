ae7b72d63198faede3c5585dcba67b1bd6c7cfd3
Cedric <mrlee.truecoder@gmail.com>
2021-09-07 16:47:36 -0500

    Initial Commit.

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,62 +6,78 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
-        public void Add(int num1, int num2, int num3, int expected)
+        [InlineData(1,2,3,6)]
+        [InlineData(5,5,5,15)]
+        public void Add(int one, int two, int three, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Add(one, two, three);
 
             //Assert
+            Assert.Equal(expected, actual);
+
 
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(5, 5, 0)]
+        public void Subtract(int menuend, int subtrahend, int expected)
         {
             //Arrange
+            var test2 = new UnitTestMethods();
 
-            //Act
+            //Act   
+            var actual = test2.Subtract(menuend, subtrahend);
 
             //Assert
+            Assert.Equal(expected, actual);
+        }
+        [InlineData(3, 3, 9)]
+        [InlineData(2, 2, 4)]
+        [Theory]
+        public void Multiply(int one, int two, int expected)
+        {
+            //Arrange
+            var test3 = new UnitTestMethods();
 
+            //Act
+            var actual = test3.Multiply(one, two);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Multiply(int num1, int num2, int expected)
+        [InlineData(25, 5, 5)]
+        [InlineData(24, 3, 8)]
+
+
+        public void Divide(int dividend, int divisor, int expected)
         {
             //Arrange
+            var test4 = new UnitTestMethods();
 
             //Act
+            var actual = test4.Divide(dividend, divisor);
 
             //Assert
-
-        }
-
-        [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
             //Arrange
+            //var test5 = new UnitTestMethods();
 
             //Act
+            //var actual = test5.UnitTestMethods
 
             //Assert
-
+            //Assert.Equal(expected, actual);
         }
 
         [Fact]
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,20 +3,33 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        //DONE-- Make sure your references are set properly
+        //DONE--Right-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
         // Create a Subtract method that passes 2 integers
+        public int Subtract(int first, int seccond)
+        {
+            return first - seccond;
+        }
             // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int one, int two)
+        {
+            return one * two;
+        }
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int dividend, int divisor)
+        {
+            return dividend / divisor; 
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
